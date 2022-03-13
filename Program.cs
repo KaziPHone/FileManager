51309b519c301a2541d35ae892f9f096a17c4c04
KaziPHone <kazimirskiyyy@icloud.com>
2022-03-13 10:18:37 +0300

    JIRA #GB-26

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Drawing;
+
+namespace FileManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
